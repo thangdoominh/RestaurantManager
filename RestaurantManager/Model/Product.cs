f063279941cc2785aa81aef22195b8bcf02aa3c1
Unknown <dominhthang1999@gmail.com>
2018-11-15 00:42:32 +0700

    Initial Login Module

---
+++ RestaurantManager/Model/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManager.Model
+{
+    class Product
+    {
+        public String productId;
+        public String name;
+        public String price;
+    }
+}
